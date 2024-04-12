6753fe5b14515cd9f727e58ad209684160dd8972
godpoh <jeremy12troll@gmail.com>
2024-04-11 18:53:01 -0600

    gd

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import random
 def show_main_menu():
-    print("----Bienvenido al juego POKER HOLD'EM----")
-    print("1- Iniciar Juego")
-    print("2- Mostrar puntuaciones")
-    print("3- Salir")
-    print("-----------------------------------------")
-
+    print("|------------------------------------------|")
+    print("|    Bienvenido al juego POKER HOLD'EM     |")
+    print("|        1. Iniciar Juego                  |")
+    print("|        2. Mostrar puntuaciones           |")
+    print("|        3. Salir                          |")
+    print("|------------------------------------------|")
+def main():
+    while True:
+        show_main_menu()
+        selection = input("Ingrese la opcion que desee: ")
+        options_menu(selection)
+def options_menu(selection):
+    if selection == "1":
+        start_game()
+    elif selection == "2":
+        show_califications()
+    elif selection == "3":
+        exit_game()
+    else:
+        print("\nOpcion invalida. Intente de nuevon\n")
 def start_game():
     shuffle_deck = create_shuffle_deck()
     initial_deal_cards(shuffle_deck)
@@ -14,21 +28,12 @@ def start_game():
 def show_califications():
     pass
 
-
 def exit_game():
     print("Saliendo del sistema")
     exit()
 
 
-def options_menu(selection):
-    if selection == "1":
-        start_game()
-    elif selection == "2":
-        show_main_menu()
-    elif selection == "3":
-        exit_game()
-    else:
-        print("Opcion invalida. Intente de nuevo")
+
 
 
 
@@ -52,7 +57,6 @@ def create_shuffle_deck():
         deck = [card.strip() for card in cards]
         random.shuffle(deck)
         return deck
-
 def initial_deal_cards(deck):
     players = {}
     player_name = input("Ingrese su nombre: ")
@@ -73,8 +77,6 @@ def initial_deal_cards(deck):
         table.append(deck.pop())
 
     return players, table
-
-
 def chip_conversion(player_name):
     denominations = {
         "blanca": 1,
@@ -97,20 +99,20 @@ def chip_conversion(player_name):
         players_chips["blanca"] += remaining_chips // min_denomination
 
     return players_chips
-
-
 def show_initial_chips(player_name, initial_chips ):
-    print(f"{player_name}, tiene las siguentes fichas: ")
+    print("\n|------------------------------------------|")
+    print(f"|{player_name}, tiene las siguentes fichas|")
+    print("|------------------------------------------|")
     for denomination, count in initial_chips.items():
         print(f"{count} ficha(s) de {denomination}")
 
 def call_option_fold():
     while True:
-        print("------------------------------")
-        print("1. Call: ")
-        print("2. Raise")
-        print("3. Fold")
-        print("------------------------------")
+        print("|------------------------------------------|")
+        print("|                1. Call                   |")
+        print("|                2. Raise                  |")
+        print("|                3. Fold                   |")
+        print("|------------------------------------------|")
 #   PUEDE SER CALL, CHECK Y ALL IN EN PRIMER RONDA, AL ACABAR PRIMER RONDA SE MUESTREN 3 CARTAS,
         option = input("Ingrese el proximo movimiento: ")
 
@@ -149,20 +151,5 @@ def handle_blinds():
 
 def all_in():
     pass
-
-
-
-
-def main():
-    while True:
-        show_main_menu()
-        selection = input("Ingrese la opcion que desee: ")
-        options_menu(selection)
-
-
-
-
-
-
 
 main()
