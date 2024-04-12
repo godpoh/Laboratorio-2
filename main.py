9de2a4eb325bb737c6379b0d689dd9ffb8e79757
godpoh <jeremy12troll@gmail.com>
2024-04-11 23:50:57 -0600

    gd

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ def options_menu(selection):
         exit_game()
     else:
         print("\nOpcion invalida. Intente de nuevon\n")
+
 def start_game():
     shuffle_deck = create_shuffle_deck()
     initial_deal_cards(shuffle_deck)
-    call_option_fold()
+    call_option_fold("1")
 
 def show_califications():
     pass
@@ -57,12 +58,19 @@ def create_shuffle_deck():
         deck = [card.strip() for card in cards]
         random.shuffle(deck)
         return deck
+
+bot = {
+    "name": "Sheldon Cooper",
+    "chips": 500,
+    "cards": []
+}
+
 def initial_deal_cards(deck):
     players = {}
     player_name = input("Ingrese su nombre: ")
 
     players[player_name] = {"fichas": 500, "cartas": []}
-    players["Sheldon Copper"] = {"fichas": 500, "cartas": []}
+    players[bot["name"]] = {"fichas": bot["chips"], "cartas": []}
 
     for player, data in players.items():
         show_initial_chips(player, chip_conversion(data["fichas"]))
@@ -102,14 +110,19 @@ def chip_conversion(player_name):
 def show_initial_chips(player_name, initial_chips ):
     print("\n|------------------------------------------|")
     print(f"|{player_name}, tiene las siguentes fichas|")
+    print("|               TOTAL ($500)               |")
     print("|------------------------------------------|")
     for denomination, count in initial_chips.items():
         print(f"{count} ficha(s) de {denomination}")
 
-def call_option_fold():
+
+def call_option_fold(last_move):
     while True:
         print("|------------------------------------------|")
-        print("|                1. Call                   |")
+        if last_move == "1":
+            print("|                1. Call                   |")
+        else:
+            print("|                1. Check                |")
         print("|                2. Raise                  |")
         print("|                3. Fold                   |")
         print("|------------------------------------------|")
@@ -117,7 +130,10 @@ def call_option_fold():
         option = input("Ingrese el proximo movimiento: ")
 
         if option == "1":
-            call()
+            if last_move == "1":
+                call()
+            else:
+                check()
         elif option == "2":
             raaise()
         elif option == "3":
@@ -125,10 +141,26 @@ def call_option_fold():
         else:
             print("Opcion invalida")
 
+def turn_players(players, table):
+    while True:
+        if current_player == 0:
+            print("\nTu turno!\n")
+            call_option_fold("1", players[current_player])
+        else:
+            print("Turno de Sheldon Cooper")
+            sheldon_move = sheldon_decide_move(bot["cards"], table)
+            call_option_fold(sheldon_move, players[current_player])
+
+
+        current_player = (current_player + 1) % 2
+def sheldon_decide_move(sheldon_cards, table_cards):
+    return random.choice(call_option_fold(["1", "2", "3"]))
 
 def call():
     pass
 
+def check():
+    pass
 def raaise():
     pass
 
